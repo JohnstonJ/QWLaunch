8fa24d9d4fe47bc741fc47e2c6b8243ce444f9ee
James Johnston <johnstonj.public@codenest.com>
2015-08-14 04:45:03 +0000

    Added QWLaunch project.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+using System.Windows.Media;
+
+// Don't allow WPF to declare us DPI aware
+[assembly: DisableDpiAwareness]
+
+namespace QWLaunch {
+	class Program {
+		// Avoid initializing any system libraries like WPF or WinForms by using Win32 API directly...
+		[System.Runtime.InteropServices.DllImport("user32.dll", CharSet = System.Runtime.InteropServices.CharSet.Auto)]
+		public static extern int MessageBox(IntPtr hWnd, String text, String caption, int options);
+		[STAThread]
+		public static int Main(string[] args) {
+			try {
+				Assembly asm = Assembly.Load("qw");
+				Type t = asm.GetType("QuickenWindow.Program");
+				MethodInfo m = t.GetMethod("Main", BindingFlags.NonPublic | BindingFlags.Static);
+				return (int)m.Invoke(null, null);
+			} catch (Exception ex) {
+				MessageBox(IntPtr.Zero, ex.ToString(), "QWLaunch error", 0);
+				return 1;
+			}
+		}
+	}
+}
